84bc835b2983007774bec84bdabc71558331b815
James Thompson <james@mavenlink.com>
2020-04-02 20:35:55 -0600

    Add subnet to VPC for Elastic Cache clusters.

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -18,3 +18,16 @@ resource "aws_vpc" "vpc" {
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
+
+resource "aws_subnet" "cache" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Team = var.team
+  }
+}
+
+output "cache_subnet_id" {
+  value = aws_subnet.cache.id
+}
