6057a7388b08ca3f2908a4b49b275393a36a59c2
James Thompson <james@mavenlink.com>
2020-04-02 20:38:31 -0600

    Add redis setup in Elastic Cache module.

---
+++ modules/cache/redis.tf
@@ -0,0 +1,18 @@
+resource "aws_elasticache_parameter_group" "redis-param-group" {
+  name   = "redis-cache-params"
+  family = "redis3.2"
+}
+
+resource "aws_elasticache_cluster" "snowpeople-redis" {
+  cluster_id = "${var.team}-${var.environment}-redis"
+  engine = "redis"
+  engine_version = "3.2.4"
+  node_type = "cache.m4.large"
+  num_cache_nodes = 1
+  parameter_group_name = "redis-cache-params"
+  subnet_group_name = "cache-subnet"
+
+  tags = {
+    Team = var.team
+  }
+}
