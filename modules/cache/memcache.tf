f5d6095106a8a7321bd3dfb93c5a40b9e28bf614
James Thompson <james@mavenlink.com>
2020-04-03 10:08:50 -0600

    Replace literal value with resource value for cache subnet name.

--- modules/cache/memcache.tf
+++ modules/cache/memcache.tf
@@ -4,12 +4,12 @@ resource "aws_elasticache_parameter_group" "memcache-param-group" {
 }
 
 resource "aws_elasticache_cluster" "memcache" {
-  cluster_id = "${var.team}-${var.environment}-memcache"
-  engine = "memcached"
-  node_type = "cache.m4.large"
-  num_cache_nodes = 2
-  parameter_group_name = "memcache-params"
-  subnet_group_name = "cache-subnet"
+  cluster_id            = "${var.team}-${var.environment}-memcache"
+  engine                = "memcached"
+  node_type             = "cache.m4.large"
+  num_cache_nodes       = 2
+  parameter_group_name  = "memcache-params"
+  subnet_group_name     = aws_elasticache_subnet_group.cache-subnet.name
 
   tags = {
     Team = var.team
--- modules/cache/redis.tf
+++ modules/cache/redis.tf
@@ -4,13 +4,13 @@ resource "aws_elasticache_parameter_group" "redis-param-group" {
 }
 
 resource "aws_elasticache_cluster" "snowpeople-redis" {
-  cluster_id = "${var.team}-${var.environment}-redis"
-  engine = "redis"
-  engine_version = "3.2.4"
-  node_type = "cache.m4.large"
-  num_cache_nodes = 1
-  parameter_group_name = "redis-cache-params"
-  subnet_group_name = "cache-subnet"
+  cluster_id            = "${var.team}-${var.environment}-redis"
+  engine                = "redis"
+  engine_version        = "3.2.4"
+  node_type             = "cache.m4.large"
+  num_cache_nodes       = 1
+  parameter_group_name  = "redis-cache-params"
+  subnet_group_name     = aws_elasticache_subnet_group.cache-subnet.name
 
   tags = {
     Team = var.team
