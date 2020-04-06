b6a51029db884e7edf23dfc54c9a34da9121d43e
James Thompson <james@mavenlink.com>
2020-04-05 18:50:03 -0600

    Bring S3 bucket for Terraform state under management.

--- terraform.tf
+++ terraform.tf
@@ -5,3 +5,32 @@ terraform {
     region = "us-west-2"
   }
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "snowpeople-terraform-staging"
+  acl           = "private"
+  force_destroy = false
+  region        = "us-west-2"
+  request_payer = "BucketOwner"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Team        = local.team
+    Environment = local.environment
+  }
+}
